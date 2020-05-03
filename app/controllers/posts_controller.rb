6ea8d8fd27ec735e3348f7731f3ee796feb7b4a4
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-05-03 23:11:30 +1000

    fixed user UI buttons, and restrictions to editing post only signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :post_id, only: [:show, :destroy,:update,:edit]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @posts = Post.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get '/about', to: 'pages#about', as: "about"
   resources :posts do
     resources :comments

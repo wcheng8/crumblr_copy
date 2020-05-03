e8cf2025a038efe3908a1f550dc10546e05acea9
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-05-03 18:12:49 +1000

    edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :post_id, only: [:show, :destroy,:update]
+  before_action :post_id, only: [:show, :destroy,:update,:edit]
   def index
     @posts = Post.all
   end
@@ -21,15 +21,22 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def destroy
-  end
-
+  
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
-
+  
   def show
   end
-
+  
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
   private
 
   def post_params
